e9c48c2c60e66cf7cd2fd3a765ffc4387a8b6d83
Seaon Shin <seaonnah@seaons-macbook.local>
2018-05-12 09:39:54 -0700

    Initial Commit

---
+++ MovieIndex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieIndex
+//
+//  Created by Seaon Shin on 5/12/18.
+//  Copyright © 2018 Seaon Shin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
